48ec51aa221db071c91a3c4894327f8330f6ba92
Kelly de Vries <kdv@me.com>
2015-02-18 12:12:15 -0800

    Add css file

---
+++ cashRegister.php
@@ -0,0 +1,24 @@
+<?php
+	$itemPrice = $_GET["itemPrice"];
+	$prices = array();
+?>
+
+
+<!DOCTYPE hmtl>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<title>Cash Register Receipt Template</title>
+</head>
+	<body>
+		<h1 class="jumbotron">Cash Register Receipt Template</h1>
+		<div id="contact-info">			
+			<h4>My Super Cool Business</h4>
+			<h4>Phone number here</h4>		
+			<address>1234 Cool Moe Dee Street<br> This Town, ST  56789</address>
+		</div>
+
+
+	</body>
+</html>
