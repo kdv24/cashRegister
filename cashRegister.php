a4d4d302b3a7471cd0c67c7be3c48cf03ce55735
Kelly de Vries <kdv@me.com>
2015-02-19 08:37:25 -0800

    Add arrays for  and .

--- cashRegister.php
+++ cashRegister.php
@@ -1,8 +1,22 @@
 <?php
-	$itemName = $_GET["itemName"];
-	$itemPrice = $_GET["itemPrice"];
-	$prices = array($itemPrice);
-	$itemQuantity = $_GET["itemQuantity"];
+	$itemName1 = $_GET["itemName1"];
+	$itemPrice1 = $_GET["itemPrice1"];
+	$itemQuantity1 = $_GET["itemQuantity1"];
+	$itemName2 = $_GET["itemName2"];
+	$itemPrice2 = $_GET["itemPrice2"];
+	$itemQuantity2 = $_GET["itemQuantity2"];
+	$itemName3 = $_GET["itemName3"];
+	$itemPrice3 = $_GET["itemPrice3"];
+	$itemQuantity3 = $_GET["itemQuantity3"];
+	$itemName4 = $_GET["itemName4"];
+	$itemPrice4 = $_GET["itemPrice4"];
+	$itemQuantity4 = $_GET["itemQuantity4"];
+	$itemName5 = $_GET["itemName5"];
+	$itemPrice5 = $_GET["itemPrice5"];
+	$itemQuantity5 = $_GET["itemQuantity5"];
+	$names = array($itemName1, $itemName2, $itemName3, $itemName4, $itemName5);	
+	$prices = array($itemPrice1, $itemPrice2, $itemPrice3, $itemPrice4, $itemPrice5);
+	$Quantity = array($itemQuantity1, $itemQuantity2, $itemQuantity3, $itemQuantity4, $itemQuantity5);
 ?>
 
 
@@ -22,7 +36,11 @@
 				<address>1234 Cool Moe Dee Street<br> This Town, ST  56789</address>
 			</div>
 			<div id="receipt-output">
-				<?php echo "Item ordered: " . "<span class='itemName'>" . $itemName . "</span>"; ?>
+				<?php
+				foreach($names as $name){
+				 echo "Item ordered: " . "<span class='itemName'>" . $name . "</span>"; 
+				}
+				?>
 				<br>
 				<div class="priceandquantity">
 				<?php echo "Price of item: " . "<span class='itemList'>" . $itemPrice . "</span>"; ?>
